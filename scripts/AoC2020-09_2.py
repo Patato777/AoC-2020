a0e5289818ad19460815b6c775a723d62073e0ce
Patato777 <jean.sma@free.fr>
2020-12-09 19:46:12 +0100

    Optimising my FIFO queue use

--- scripts/AoC2020-09_2.py
+++ scripts/AoC2020-09_2.py
@@ -3,10 +3,13 @@ import functools
 weakness = 373803594
 with open('../inputs/AoC2020-09.txt','r') as file :
     fifou2 = list()
-    while sum(fifou2) != weakness :
+    total = 0
+    while total != weakness :
         k = int(file.readline())
-        while sum(fifou2)+k > weakness :
-            fifou2.pop(0)
+        while total+k > weakness :
+            total -= fifou2.pop(0)
+        total += k
         fifou2.append(k)
 
+#If I wanted to use fifou2 really as a FIFO queue, I should calculate min and max as programme goes along
 print(min(fifou2)+max(fifou2))
